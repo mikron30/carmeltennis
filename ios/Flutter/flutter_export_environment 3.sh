601d1191c506305f5c4ca72b81853e147c617642
דולב בן ציוני <dolevbz1@h-mhsb-nyyd-sl-dwlb.local>
2024-09-15 18:07:50 +0300

    update

---
+++ ios/Flutter/flutter_export_environment 3.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/users/dolevbz1/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/dolevbz1/Desktop/carmeltennis"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=3"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/dolevbz1/Desktop/carmeltennis/.dart_tool/package_config.json"
